f66b3717903b81203032a993072f5431575d0e67
Alexander Eric <70110009+alexandereric995@users.noreply.github.com>
2021-02-17 22:46:32 +0800

    Create open.ai.print.text.js

---
+++ open.ai.print.text.js
@@ -0,0 +1,17 @@
+<script>
+We’re releasing an API for accessing new AI models developed by OpenAI. Unlike most AI systems which are designed for one use-case, the API today provides a general-purpose “text in, text out” interface, allowing users to try it on virtually any English language task. You can now request access in order to integrate the API into your product, develop an entirely new application, or help us explore the strengths and limits of this technology. The road to making AI safe and useful is long and challenging, but with the support of the developer community we expect to get there much faster than working alone.
+{
+    "choices": [
+        {
+        "finish_reason": "stop",
+        "index": 0,
+        "logprobs": null,
+        "text": "  The road to making AI safe and useful is long and challenging, but with the support of the developer community we expect to get there much faster than working alone."
+        }
+    ],
+    "created": 1591591436,
+    "id": "cmpl-QMXsObreAfysApcgi5Kw1uwp",
+    "model": "davinci:2020-05-03",
+    "object": "completion"
+}      
+    </script>
