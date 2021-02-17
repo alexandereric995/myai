9c6ab892890a0d621079cf43ec5257ef7148d693
Alexander Eric <70110009+alexandereric995@users.noreply.github.com>
2021-02-17 21:43:55 +0800

    Create notify.login.js

---
+++ notify.login.js
@@ -0,0 +1,15 @@
+<script language="javascript">
+<!--//
+/*notify users*/
+function pasuser(form) {
+if (form.id.value=="JavaScript") { 
+if (form.pass.value=="Kit") {              
+location="page2.html" 
+} else {
+alert("Invalid Password")
+}
+} else {  alert("Invalid UserID")
+}
+}
+//-->
+</script>
